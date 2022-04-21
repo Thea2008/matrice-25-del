d59b9b5833c11f82f0c40ca931f2bc5ed4ddb9ea
Thea2008 <103181295+Thea2008@users.noreply.github.com>
2022-04-21 11:03:07 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,57 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
